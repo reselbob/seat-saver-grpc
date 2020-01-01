6a9f9cd28c6226a8a196902535018575d9ec2dba
Bob Reselman <reselbob@gmail.com>
2020-01-01 12:08:15 -0800

    adding JSON Schema validation

--- test/basic-grpc-tests.js
+++ test/basic-grpc-tests.js
@@ -1,6 +1,7 @@
 const SERVER_URL = 'localhost:50051';
 const PROTO_PATH = process.cwd() + '/proto/seatsaver.proto';
-const faker = require('faker');
+const testHelpers = require('./test-helpers');
+const validation = require('../validation');
 
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
@@ -18,42 +19,13 @@ const seatsaver = grpc.loadPackageDefinition(packageDefinition).seatsaver;
 const client = new seatsaver.SeatSaverService(SERVER_URL,
     grpc.credentials.createInsecure());
 
-const chai = require('chai');
 const expect = require('chai').expect;
 const describe = require('mocha').describe;
 const it = require('mocha').it;
 
-const getRandomCustomerSync = () => {
-    const obj = {};
-    obj.firstName = faker.name.firstName();
-    obj.lastName = faker.name.lastName();
-    const email = `${obj.firstName}.${obj.lastName}@${faker.internet.domainName()}`;
-    obj.email = email;
-
-    return obj;
-};
-
 const sample = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)]
 };
-
-const getRandomSeatCustomerSync = () => {
-    const obj = {};
-    obj.firstName = faker.name.firstName();
-    obj.lastName = faker.name.lastName();
-    const email = `${obj.firstName}.${obj.lastName}@${faker.internet.domainName()}`;
-    obj.email = email;
-    obj.created = null;
-    obj.message = null;
-
-    return obj;
-};
-
-
-let testSeat = null;
-
-
-
 
 describe('Basic Grpc Tests: ', () => {
     it('Can Ping Server ', function (done) {
@@ -74,7 +46,7 @@ describe('Basic Grpc Tests: ', () => {
         call.on('data', function (result) {
             expect(result).to.be.an('object');
             const obj = {seat: sample(result.seats), venueId: result.id};
-            obj.seat.customer = getRandomCustomerSync();
+            obj.seat.customer = testHelpers.getRandomCustomerSync();
             function reserveSeatCallback(error, result) {
                 if (error) {
                     console.log(error);
@@ -101,6 +73,8 @@ describe('Basic Grpc Tests: ', () => {
     it('Can Get Seats in Venue', function (done) {
         const call = client.GetSeats({});
         call.on('data', function (result) {
+            const validated = validation.validateSeatSync(result);
+            expect(validated.error.length).to.equal(0);
             expect(result).to.be.an('object');
             expect(result.number).to.be.a('string');
             expect(result.section).to.be.a('string');
@@ -149,8 +123,12 @@ describe('Basic Grpc Tests: ', () => {
         const call = client.GetVenues({});
         call.on('data', function (result) {
             expect(result).to.be.an('object');
+
+            const validated = validation.validateVenueSync(result);
+            expect(validated.errors.length).to.equal(0);
+
             const obj = {seat: sample(result.seats), venueId: result.id};
-            obj.seat.customer = getRandomCustomerSync();
+            obj.seat.customer = testHelpers.getRandomCustomerSync();
             function buySeatCallback(error, result) {
                 if (error) {
                     console.log(error);
